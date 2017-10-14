d4f40e3f34769d867c856c4a55b444cda1fd43b4
munens <kaumbuthom@yahoo.com>
2017-10-13 20:41:59 -0700

    Sec. 2, lec. 22-24: Special CSS selectors , :host and function form of :host

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
-  styleUrls: ['./quote.component.css'] // here the styles file is simply referenced. 
+  styleUrls: ['./quote.component.css'], // here the styles file is simply referenced. 
+  encapsulation: ViewEncapsulation.Emulated // .Emulated is default, .Native is the native shadow DOM that only some browsers have, .none turns view encapsulation off and the styling properties can be used globally rather than on the current component.
 })
 export class QuoteComponent implements OnInit {
 
